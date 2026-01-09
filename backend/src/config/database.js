3a003361835acf8794dae452f47245aa6653bab4
unknown <w.rickysingha@gmail.com>
2026-01-09 13:50:02 +0530

    Some changes for render

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,22 +1,26 @@
 import {Pool} from 'pg';
 
-const requiredEnvs = ['DB_HOST','DB_NAME','DB_PORT','DB_USER','DB_PASSWORD'];
-const missingEnvs = requiredEnvs.filter(varName => !process.env[varName]);
-if (missingEnvs.length > 0){
-    console.error(`Missing required environment variables: ${missingEnvs.join(', ')}`);
-    process.exit(1);
-}
-
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ,
-    database: process.env.DB_NAME ,
-    user: process.env.DB_USER ,
-    password: process.env.DB_PASSWORD ,
-    max: 20,
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
     idleTimeoutMilli: 30000,
     connectionTimeOutMilli: 2000,
+  // This logic checks if we are on Render; if yes, it forces SSL
+  ssl: process.env.DATABASE_URL.includes('render.com') ? {
+    rejectUnauthorized: false
+  } : false
 });
+
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT ,
+//     database: process.env.DB_NAME ,
+//     user: process.env.DB_USER ,
+//     password: process.env.DB_PASSWORD ,
+//     max: 20,
+//     idleTimeoutMilli: 30000,
+//     connectionTimeOutMilli: 2000,
+// });
 
 pool.on('connect',()=>{
     console.log('Connected to PostgreSql');
